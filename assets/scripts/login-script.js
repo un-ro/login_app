200acce4a302ac709ce8d2ab2e800d545cf8c8ca
Unero <ramadhanbhg@gmail.com>
2021-08-22 12:20:20 +0700

    Kriteria 4

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,16 +5,28 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** 
+ * Membuat variabel untuk form Login
+ * @constant {HTMLElement}
+ */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+ * Membuat variabel email dan password untuk check input user saat login
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+   * Membuat variabel untuk menyimpan nilai email dan password yang user inputkan
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
